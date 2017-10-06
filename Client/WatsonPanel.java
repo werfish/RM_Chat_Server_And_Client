53b0863ed55d26df91a1fcc1448197cc1d0efe81
werfish <werfish1@gmail.com>
2017-10-06 12:14:15 +0200

    Create and planned out the watson panel
    Created and Planned out the Watson panel.

---
+++ Client/WatsonPanel.java
@@ -0,0 +1,11 @@
+public class WatsonPanel exteds JPanel {
+  //needs several controls
+  //A label to say IBM Watson
+  //A label to say Language:
+  //A list view to show all available langues
+  // Translate button
+
+  public WatsonPanel() {
+  
+  }
+}
