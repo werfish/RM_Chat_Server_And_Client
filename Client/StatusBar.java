2a4cfb2458330c8cb4e1123e340f84e22ef23371
werfish <werfish1@gmail.com>
2017-10-06 12:09:01 +0200

    created StatusBar.java
    Created and Planned out the StatusBar class.

---
+++ Client/StatusBar.java
@@ -0,0 +1,24 @@
+public class StatusBar extends JPanel {
+//This class will be used to display the connection status, display messages when it is trying to reconnect etc
+//This is the first class to implement the 
+  private JLabel connLabel;
+  private JLabel msgLabel;
+
+  public StatusJLabel() {
+    this.addLayout(new BoxLayout());
+  }
+  
+  public void setNotConnected() {
+      
+  }
+  
+  public void setConnected() {
+  
+  }
+  
+  //the message should be displayed only for max 2-3 seconds
+  public void display message() {
+  
+  }
+
+}
