1442fda7813ae1c4aeeb80d91a0b537d24925f5a
Robert Mazurowski <werfish1@gmail.com>
2017-10-07 14:05:13 +0200

    Changed some errors in status bar

--- Client/StatusBar.java
+++ Client/StatusBar.java
@@ -1,11 +1,15 @@
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 public class StatusBar extends JPanel {
 //This class will be used to display the connection status, display messages when it is trying to reconnect etc
 //This is the first class to implement the 
   private JLabel connLabel;
   private JLabel msgLabel;
 
-  public StatusJLabel() {
-    this.addLayout(new BoxLayout());
+  public StatusBar() {
+    this.setLayout(new BoxLayout(this,BoxLayout.PAGE_AXIS));
   }
   
   public void setNotConnected() {
@@ -17,7 +21,7 @@ public class StatusBar extends JPanel {
   }
   
   //the message should be displayed only for max 2-3 seconds
-  public void display message() {
+  public void displayMessage() {
   
   }
 
