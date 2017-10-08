721b94c1921dc6896f8060431dcd9130c62122a1
Robert Mazurowski <werfish1@gmail.com>
2017-10-08 18:35:16 +0200

    Planning out the TEngine functions which will be used on the server

--- TEngine/TEngine.java
+++ TEngine/TEngine.java
@@ -1,5 +1,47 @@
 package TEngine;
 
-public class TEngine {
+import java.util.List;
 
+import Client.ConnectionHandler;
+
+public class TEngine {
+	//TEngine is the main class of the translation engine which the server uses to communicate with the APIs
+	//This class is a singleton
+	
+	private static TEngine singleton;
+	
+	//The TranslationService instance
+	TranslationService service;
+	
+	//CONFIG
+	final static int AMOUNT_OF_THREADS = 3; //;Amount of threads which handle API calls at the same time
+	
+	private TEngine() {
+		
+	}
+	
+	public TEngine getInstance() {
+		if(singleton == null){
+			synchronized (TEngine.class){
+				singleton  = new TEngine();
+			}
+		}
+		return singleton;
+	}
+	
+	public void start() {
+		
+	}
+	
+	public void stop() {
+		
+	}
+	
+	public void getTranslationRequest(TranslationRequest request) {
+		
+	}
+	
+	public void getTranslationRequest(List<TranslationRequest> reqList) {
+		
+	}
 }
